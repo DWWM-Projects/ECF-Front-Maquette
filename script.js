6810894423cf6f746167eb65df61cd39721c8961
Antrâxx <h.julien62@gmail.com>
2022-06-28 14:25:40 +0200

    Javascript fait

--- script.js
+++ script.js
@@ -43,3 +43,32 @@ $("#close").click(function () {
     })
 
 })
+
+var min = 1;
+var max = 99999;
+$("#randValue1").text("94000+")
+$("#randValue2").text("11000+")
+$("#randValue3").text("1500+")
+
+
+// function random(min, max) {
+//     var randomNumber = Math.floor(Math.random() * (max - min)) + min;
+//     console.log(randomNumber)
+// }
+
+$("#randomButton").click(function () {
+    var randomNumber = Math.floor(Math.random() * (max - min)) + min;
+    $("#randValue1").text(randomNumber + "+")
+    var randomNumber = Math.floor(Math.random() * (max - min)) + min;
+    $("#randValue2").text(randomNumber + "+")
+    var randomNumber = Math.floor(Math.random() * (max - min)) + min;
+    $("#randValue3").text(randomNumber + "+")    
+})
+
+$("#submitButton").click(function () {
+    var inputValue = $("#input").val();
+    $("#resultInput").text("Merci " + inputValue);
+    $("#input").addClass("hide");
+    $("#submitButton").addClass("hide");
+    $("#resultInput").removeClass("hide");
+})
